d9b41c99fe59cfcb8b397d4bb36d7e6a556e4030
Ricardo Cravo <153399311+RPG-Study@users.noreply.github.com>
2024-08-21 10:37:43 +0200

    feat: class playerexp defines xp on all skills of player. init when class player is called.

---
+++ Python/Advanced_Runescape/player_exp.py
@@ -0,0 +1,12 @@
+class PlayerExp:
+    '''
+    Creates Player class.
+    '''
+    def __init__(self) -> None:
+        self.health_xp: int = 0
+        self.attack_xp: int = 0
+        self.strength_xp: int = 0
+        self.defense_xp: int = 0
+
+if __name__ == '__main__':
+    print('Please run rs3.py instead.')
