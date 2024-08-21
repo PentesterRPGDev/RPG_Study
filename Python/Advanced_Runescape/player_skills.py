fff8a588bb2d11cf1f8bd98f5def760c17cf73de
Ricardo Cravo <153399311+RPG-Study@users.noreply.github.com>
2024-08-21 10:35:59 +0200

    feat: playerSkills, initiate this class giving fixed lvl to the skills

---
+++ Python/Advanced_Runescape/player_skills.py
@@ -0,0 +1,12 @@
+class PlayerSkills:
+    '''
+    Creates Player class.
+    '''
+    def __init__(self) -> None:
+        self.health: int = 10
+        self.attack: int = 1
+        self.strength: int = 1
+        self.defense: int = 1
+
+if __name__ == '__main__':
+    print('Please run rs3.py instead.')
